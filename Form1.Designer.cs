ff6b0b777c9494d5fc4e375de474d54462ed248c
root <mgiraudo@ies21.edu.ar>
2022-11-07 11:05:53 -0300

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace pruPonssaSP3ER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.mrcDatos = new System.Windows.Forms.GroupBox();
+            this.lstMarca = new System.Windows.Forms.ComboBox();
+            this.lblOrigen = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.optNacional = new System.Windows.Forms.RadioButton();
+            this.optImportado = new System.Windows.Forms.RadioButton();
+            this.mrcDatos.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(27, 54);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(40, 13);
+            this.lblMarca.TabIndex = 0;
+            this.lblMarca.Text = "Marca:";
+            // 
+            // mrcDatos
+            // 
+            this.mrcDatos.Controls.Add(this.optImportado);
+            this.mrcDatos.Controls.Add(this.optNacional);
+            this.mrcDatos.Controls.Add(this.lblDescripcion);
+            this.mrcDatos.Controls.Add(this.txtDescripcion);
+            this.mrcDatos.Controls.Add(this.lblNumero);
+            this.mrcDatos.Controls.Add(this.txtNumero);
+            this.mrcDatos.Controls.Add(this.lblOrigen);
+            this.mrcDatos.Controls.Add(this.lstMarca);
+            this.mrcDatos.Controls.Add(this.lblMarca);
+            this.mrcDatos.Location = new System.Drawing.Point(12, 12);
+            this.mrcDatos.Name = "mrcDatos";
+            this.mrcDatos.Size = new System.Drawing.Size(547, 238);
+            this.mrcDatos.TabIndex = 1;
+            this.mrcDatos.TabStop = false;
+            this.mrcDatos.Text = "Ingresar datos del repuesto";
+            // 
+            // lstMarca
+            // 
+            this.lstMarca.FormattingEnabled = true;
+            this.lstMarca.Items.AddRange(new object[] {
+            "P",
+            "F",
+            "R"});
+            this.lstMarca.Location = new System.Drawing.Point(78, 51);
+            this.lstMarca.Name = "lstMarca";
+            this.lstMarca.Size = new System.Drawing.Size(63, 21);
+            this.lstMarca.TabIndex = 1;
+            // 
+            // lblOrigen
+            // 
+            this.lblOrigen.AutoSize = true;
+            this.lblOrigen.Location = new System.Drawing.Point(228, 54);
+            this.lblOrigen.Name = "lblOrigen";
+            this.lblOrigen.Size = new System.Drawing.Size(41, 13);
+            this.lblOrigen.TabIndex = 2;
+            this.lblOrigen.Text = "Origen:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(48, 141);
+            this.txtNumero.MaxLength = 6;
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(103, 20);
+            this.txtNumero.TabIndex = 4;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(45, 114);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(106, 13);
+            this.lblNumero.TabIndex = 5;
+            this.lblNumero.Text = "Número de repuesto:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(311, 111);
+            this.txtDescripcion.MaxLength = 50;
+            this.txtDescripcion.Multiline = true;
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(209, 88);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(229, 114);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 7;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // optNacional
+            // 
+            this.optNacional.AutoSize = true;
+            this.optNacional.Location = new System.Drawing.Point(289, 52);
+            this.optNacional.Name = "optNacional";
+            this.optNacional.Size = new System.Drawing.Size(67, 17);
+            this.optNacional.TabIndex = 8;
+            this.optNacional.TabStop = true;
+            this.optNacional.Text = "Nacional";
+            this.optNacional.UseVisualStyleBackColor = true;
+            // 
+            // optImportado
+            // 
+            this.optImportado.AutoSize = true;
+            this.optImportado.Location = new System.Drawing.Point(362, 52);
+            this.optImportado.Name = "optImportado";
+            this.optImportado.Size = new System.Drawing.Size(72, 17);
+            this.optImportado.TabIndex = 9;
+            this.optImportado.TabStop = true;
+            this.optImportado.Text = "Importado";
+            this.optImportado.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(571, 393);
+            this.Controls.Add(this.mrcDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.mrcDatos.ResumeLayout(false);
+            this.mrcDatos.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.GroupBox mrcDatos;
+        private System.Windows.Forms.RadioButton optImportado;
+        private System.Windows.Forms.RadioButton optNacional;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblOrigen;
+        private System.Windows.Forms.ComboBox lstMarca;
+    }
+}
+
